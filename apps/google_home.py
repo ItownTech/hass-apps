296c8420f327c1dfb0322973663bd0ff50a486f4
Sriram Venkat <sriramvenkat@yahoo-inc.com>
2017-12-12 15:43:35 +0530

    Chnage Order

--- apps/google_home.py
+++ apps/google_home.py
@@ -1,23 +1,5 @@
 import appdaemon.appapi as appapi
 
-class GoogleHome(appapi.AppDaemon):
-    def initialize(self):
-        self.log("Hello from Google Home")
-        self.register_endpoint(self.api_call)
-        self.actions=self.get_app("actions")
-        self.log(self.actions)
-    def api_call(self,data):
-        intent=self.get_apiai_intent(data).lower()
-        if "." in intent:
-            intent=intent.replace(".","_")
-        slots=self.get_apiai_slot_value(data)
-        self.log("got {} and {}".format(intent,slots))
-        res=self.actions.trigger(intent,slots)
-        return self.response(res),200
-
-    def response(self,text):
-        return self.format_apiai_response(text)
-
 
 class Actions(appapi.AppDaemon):
     def initialize(self):
@@ -54,3 +36,22 @@ class Actions(appapi.AppDaemon):
             return "{} not found".format(device)
         state=self.get_state(entity_id=entity_id)
         return "{} is currently {}".format(device,state)
+
+
+class GoogleHome(appapi.AppDaemon):
+    def initialize(self):
+        self.log("Hello from Google Home")
+        self.register_endpoint(self.api_call)
+        self.actions=self.get_app("actions")
+        self.log(self.actions)
+    def api_call(self,data):
+        intent=self.get_apiai_intent(data).lower()
+        if "." in intent:
+            intent=intent.replace(".","_")
+        slots=self.get_apiai_slot_value(data)
+        self.log("got {} and {}".format(intent,slots))
+        res=self.actions.trigger(intent,slots)
+        return self.response(res),200
+
+    def response(self,text):
+        return self.format_apiai_response(text)
